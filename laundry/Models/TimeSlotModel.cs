05890f58c0f3999e5fdadcac7c8483c676efe0a6
ThamerMashniTuwaiq <thamermashni@gmail.com>
2021-04-17 00:06:06 +0300

    first commit

---
+++ laundry/Models/TimeSlotModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace laundry.Models
+{
+    public class TimeSlotModel 
+    {
+        public string name { get; set; }
+
+        public string lm { get; set; }
+
+        public DateTime timeslot { get; set; }
+
+        public DateTime regdate { get; set; }
+    }
+}
---
+++ laundry/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using laundry.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace laundry.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public TimeSlotModel timeslot { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
