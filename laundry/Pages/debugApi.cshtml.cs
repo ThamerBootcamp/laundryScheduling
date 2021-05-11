e5d77cd135411df9498fb2494bd9111bebac6fba
ThamerMashni <thamer.mashni@tuwaiq.edu.sa>
2021-05-12 02:05:05 +0300

    bug fix: free timeslots

--- laundry/Pages/debugApi.cshtml.cs
+++ laundry/Pages/debugApi.cshtml.cs
@@ -2,45 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using laundry.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace laundry.Pages
 {
-    public class FreeTimeslotsModel : PageModel
+    public class debugApiModel : PageModel
     {
         private readonly laundry.Data.TimeSlotContext _context;
 
-        public FreeTimeslotsModel(laundry.Data.TimeSlotContext context)
+        public debugApiModel(laundry.Data.TimeSlotContext context)
         {
             _context = context;
         }
 
         private ValidTimeSlots validTs = new ValidTimeSlots();
 
-        public JsonResult OnGet(string Date,string LM)
+        public JsonResult OnGet(string Date, string LM)
         {
-            var records =new List<DateTime>();
+            var records = new List<DateTime>();
             var freeTs = new List<SelectListItem>();
             var busyTs = new List<SelectListItem>();
-            
+            var debug = new List<string>();
+
 
             if (DateTime.TryParse(Date, out var d) && !string.IsNullOrEmpty(LM))
             {
                 DateTime date = DateTime.Parse(Date);
 
-                records =  (from m in _context.TimeSlotModel
-                              where m.lm == LM && m.timeslot.Date == date.Date
-                              select m.timeslot).ToList();
+                records = (from m in _context.TimeSlotModel
+                           where m.lm == LM && m.timeslot.Date == date.Date
+                           select m.timeslot).ToList();
 
 
                 foreach (var rec in records)
                 {
-                    foreach(var vt in validTs.validTimeSlots)
+                    foreach (var vt in validTs.validTimeSlots)
                     {
                         string recTime = rec.ToString("HH:mm:ss").Substring(0, 5);
+                        //string recTime = rec.ToLongTimeString().Substring(0, 4);
+                        debug.Add("recTime: "+recTime + ", vt text: " + vt.Text);
+
                         if (recTime.Equals(vt.Text))
                         {
                             busyTs.Add(vt);
@@ -49,10 +52,10 @@ namespace laundry.Pages
                     }
                 }
 
-                freeTs =  validTs.validTimeSlots.Where(x => !busyTs.Contains(x)).ToList();
+                freeTs = validTs.validTimeSlots.Where(x => !busyTs.Contains(x)).ToList();
             }
 
-            return new JsonResult(new { data = freeTs });
+            return new JsonResult(new { records = records, busy = busyTs , free = freeTs , debug = debug});
         }
     }
 }
